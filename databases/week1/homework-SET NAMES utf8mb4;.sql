dcae321c9663c816dfc07c27b2da3a5fd31143ca
juliadavydenko <julietta12345@yahoo.com>
2023-02-23 10:43:35 +0100

    Updated homework solutions

--- databases/week1/homework-SET NAMES utf8mb4;.sql
+++ databases/week1/homework-SET NAMES utf8mb4;.sql
@@ -1,67 +1,30 @@
 USE homework1
---Select the names and phones of all users;
-USE homework1;
-SELECT email, phone FROM user;
+-- 1. Select the names and phones of all users;
+SELECT name, phone FROM user;
+-- 2. Select the name of the user with id=10;
+SELECT name FROM user WHERE id=10;
+-- 3. Find how many users exist in the database;
+SELECT COUNT(id) FROM user;
+-- 4. Select the names of the first 5 users in the database;
+SELECT name FROM user ORDER BY id ASC LIMIT 5;
+-- 5. Select the names of the last 3 users in the database;
+SELECT name FROM user ORDER BY id DESC LIMIT 3;
+-- 6. Sum all the ids in the user table;
+SELECT SUM(id) FROM user;
+-- 7. Select all users and order them alphabetically by name;
+SELECT * FROM user ORDER BY name ASC;
+-- 8. Find all tasks that include SQL either on the title or on the description; -- Note % vs _
+SELECT * FROM task WHERE title LIKE '%SQL%' OR description LIKE '%SQL%'; 
 
---Select the name of the user with id=10;
-USE homework1;
-SELECT name FROM user
-WHERE id=10;
+-- 9. Find the title of all tasks that the user Maryrose is responsible for;
+SELECT title FROM task LEFT JOIN user ON task.user_id = user.id WHERE user.name LIKE 'Maryrose%';
 
---Find how many users exist in the database;
-USE homework1;
-SELECT COUNT(id)
-FROM user;
+-- 10. Find how many tasks each user is responsible for;
+SELECT name, COUNT(user_id) FROM user JOIN task ON user_id = user.id GROUP BY user_id;
 
---Select the names of the first 5 users in the database;
-USE homework1;
-SELECT name
-FROM user
-LIMIT 5;
-
-
---Select the names of the last 3 users in the database;
-USE homework1;
-SELECT name
-FROM user
-ORDER BY id DESC
-LIMIT 3;
-
---Sum all the ids in the user table;
-USE homework1;
-SELECT SUM(id)
-FROM user;
-
---Select all users and order them alphabetically by name;
-USE homework1;
-SELECT * FROM user
-ORDER BY name;
-
---Find all tasks that include SQL either on the title or on the description;
-USE homework1;
-SELECT * FROM task
-WHERE title LIKE '%SQL%'
-OR description LIKE '%SQL%';
-
---Find the title of all tasks that the user Maryrose is responsible for;
-USE homework1;
-SELECT title FROM task
-WHERE user_id=6;
-
---Find how many tasks each user is responsible for;
-USE homework1;
-select avg(n)
-from (
-    select count(s.id) as n
-    from user u left outer join task s on u.id = s.user_id
-    group by u.id
-) as dt
-
---Find how many tasks with a status=Done each user is responsible for;
-USE homework1;
-select avg(d)
-from (
-    select count(s.id) as d
-    from user u left outer join task s on u.id = s.status_id
-    group by u.id
-) as dt
+-- 11. Find how many tasks with a status=Done each user is responsible for;
+SELECT user.id, user.name, COUNT(task.id)
+FROM user JOIN task ON task.user_id = user.id
+     JOIN status ON status.id = task.status_id
+    WHERE status.name = 'Done'
+    GROUP BY user.id
