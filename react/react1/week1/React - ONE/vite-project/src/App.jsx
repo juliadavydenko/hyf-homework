64b515a51413c953bd15fb6d95800b5c9a4e95de
juliadavydenko <julietta12345@yahoo.com>
2023-03-26 22:09:43 +0200

    Todo List

--- react/react1/week1/React - ONE/vite-project/src/App.jsx
+++ react/react1/week1/React - ONE/vite-project/src/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { UserList1 } from './Userlist'
+import './styles.css'
 
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import tasks from "./tasks.js";
 function App() {
-
   return (
-    <div className="App">
-      <div> <h1>HackYourFuture</h1>
-      <UserList1></UserList1>
-      </div>
+    <div>
+      <h1 className="heading">ToDo LIST</h1>
+      <Card
+        name={tasks[0].name}
+        img={tasks[0].imgURL}
+        date={tasks[0].date}
+        description={tasks[0].description}
+      />
+      <Card
+        name={tasks[1].name}
+        img={tasks[1].imgURL}
+       date={tasks[1].date}
+        description={tasks[1].description}
+      />
+      <Card
+        name={tasks[2].name}
+        img={tasks[2].imgURL}
+        date={tasks[2].date}
+        description={tasks[2].description}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
