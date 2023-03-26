28a831b89e2ec0d57c7982edd915b06651837322
juliadavydenko <julietta12345@yahoo.com>
2023-03-26 20:50:01 +0200

    Added project folder

---
+++ react/react1/week1/React - ONE/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { UserList1 } from './Userlist'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div> <h1>HackYourFuture</h1>
+      <UserList1></UserList1>
+      </div>
+    </div>
+  )
+}
+
+export default App
