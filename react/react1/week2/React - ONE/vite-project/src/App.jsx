293acd6d364ff9ab9e4baa6f9dbc306394668afc
juliadavydenko <julietta12345@yahoo.com>
2023-05-11 15:28:02 +0200

    Upd

--- react/react1/week2/React - ONE/vite-project/src/App.jsx
+++ react/react1/week2/React - ONE/vite-project/src/App.jsx
@@ -1,36 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './styles.css';
 
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Card";
 import tasks from "./tasks.js";
 function App() {
-  return (
-    <div>
-      <h1 className="heading">ToDo LIST</h1>
-      <Card
-        name={tasks[0].name}
-        img={tasks[0].imgURL}
-        date={tasks[0].date}
-        description={tasks[0].description}
-      />
-      <Card
-        name={tasks[1].name}
-        img={tasks[1].imgURL}
-       date={tasks[1].date}
-        description={tasks[1].description}
-      />
-      <Card
-        name={tasks[2].name}
-        img={tasks[2].imgURL}
-        date={tasks[2].date}
-        description={tasks[2].description}
-      />
-    </div>
-  );
-}
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask
+    }
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+  
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="App">
+    <div className='addTask'>
+    <input onChange={handleChange}/>
+    <button onClick={addTask}> Add task</button>
+    <div className='List'>
+      {todoList.map((task) => {
+        return (
+          <div>
+          <h1>{task.taskName}</h1>
+          <button onClick={() => deleteTask(task.id)}>Delete Task</button>
+          </div>
+          );
+      })}
+      </div>
+      </div>
+      </div>)};
+    
 export default App;
